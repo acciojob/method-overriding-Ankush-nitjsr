29cff74f3d6747c3b244ac53ebd7734f8b229511
Ankush <ankushnitjsr@gmail.com>
2023-04-11 15:00:45 +0530

    add method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        A objA = new A();
+        String answer;
+        answer = objA.meth();
+        System.out.println(answer);
+
+        B objB = new B();
+        String updatedAnswer;
+        updatedAnswer = objB.meth();
+        System.out.println(updatedAnswer);
+
+    }
 }
