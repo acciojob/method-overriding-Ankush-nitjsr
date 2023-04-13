7847836027f69ae00935292bd1826341d6f05e39
Ankush <ankushnitjsr@gmail.com>
2023-04-13 16:44:47 +0530

    master

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,17 +5,4 @@ public class Main {
         B obj = new B();
         obj.meth();
     }
-
-    public static class A {
-        public String meth(){
-            return "Invoking method from class A";
-        }
-    }
-
-    public static class B extends A {
-        @Override
-        public String meth(){
-            return "Method is overridden in Extendend class B";
-        }
-    }
 }
